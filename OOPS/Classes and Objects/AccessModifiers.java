06be9e7c6682cc114dab1d3845521c19dc6aecbc
Varis Rana <vrvarisrana123@gmail.com>
2022-01-17 18:44:49 +0530

    commit 17

--- OOPS/Classes and Objects/AccessModifiers.java
+++ OOPS/Classes and Objects/AccessModifiers.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
 public class AccessModifiers {
-
+      //PRIVATE
+      class A{  
+            private int data=40;  
+            private void msg(){System.out.println("Hello java");}  
+            }  
+              
+            public class Simple{  
+             public static void main(String args[]){  
+               A obj=new A();  
+               System.out.println(obj.data);//Compile Time Error  
+               obj.msg();//Compile Time Error  
+               }  
+            }  
 }
