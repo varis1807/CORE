2e0055dc45ee2a27a861b7299bead75db91c4f2d
Varis Rana <vrvarisrana123@gmail.com>
2022-01-17 15:33:45 +0530

    commit 17

--- OOPS/Classes and Objects/clsObj1.java
+++ OOPS/Classes and Objects/clsObj1.java
@@ -4,11 +4,14 @@ public class clsObj1 {
       public static class Hero {
             // Properties
             int health;
+            String name;
       }
 
       public static void main(String[] args) {
-//Creation of object or instance
-Hero h1=new Hero();
-System.out.println(h1.health);
+            // Creation of object or instance
+            Hero h1 = new Hero();
+            Hero n = new Hero();
+            System.out.println(h1.health);
+            System.out.println(n.name);
       }
 }
