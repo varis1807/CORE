cdd41279dafc7f7a43f0c27b80cd98fa22d257c1
Varis Rana <vrvarisrana123@gmail.com>
2022-01-18 11:33:41 +0530

    commit 18

--- OOPS/Classes and Objects/staticDynamicBinding.java
+++ OOPS/Classes and Objects/staticDynamicBinding.java
@@ -1,8 +1,27 @@
 import java.util.*;
-public class staticDynamicBinding {
-      //Static Binding
 
-      public static class superclass{
-            
+public class staticDynamicBinding {
+      // Static Binding
+      // class 1
+      public static class superclass {
+            static void print() {
+                  System.out.println("print() int superclass is called");
+            }
+      }
+
+      // class 2
+      public static class subclass extends superclass {
+            // method of inner cls
+            static void print() {
+                  System.out.println("print() in subcls is called");
+            }
+      }
+
+      public static void main(String[] args) {
+            superclass A = new superclass();
+            superclass B = new subclass();
+
+            A.print();
+            B.print();
       }
 }
