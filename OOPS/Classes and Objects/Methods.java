0c73c441b453082424f061c996e81619961e38d6
Varis Rana <vrvarisrana123@gmail.com>
2022-01-18 12:07:49 +0530

    commit 18

--- OOPS/Classes and Objects/Methods.java
+++ OOPS/Classes and Objects/Methods.java
@@ -1,4 +1,31 @@
 import java.util.*;
+
 public class Methods {
-      
+
+      // preDifine Method
+      public static void main(String[] args) {
+            // using the max() method of Math class
+            System.out.print("The maximum number is: " + Math.max(9, 7));
+      }
+
+      // User-difined Method
+
+      public static void main(String args[]) {
+            // creating Scanner class object
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            // reading value from user
+            int num = scan.nextInt();
+            // method calling
+            findEvenOdd(num);
+      }
+
+      // user defined method
+      public static void findEvenOdd(int num) {
+            // method body
+            if (num % 2 == 0)
+                  System.out.println(num + " is even");
+            else
+                  System.out.println(num + " is odd");
+      }
 }
