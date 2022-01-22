b40e924becc7668de16cb82a86d7026787438fd4
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 06:17:31 +0530

    commit 22

--- OOPS/Encapsulation/test.java
+++ OOPS/Encapsulation/test.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class test {
       public static void main(String[] args) {
             Student s = new Strudent();
-
+            s.setName("SR");
+            System.out.println(s.getName());
       }
 }
