abd0f5a0d12ae264422d52b254904d6e18b45697
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 06:28:44 +0530

    commit 22

---
+++ OOPS/Encapsulation/writeOnly.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class writeOnly {
+      // private data member
+      private static Sring college;
+
+      // setter methode for college
+      public void setCollege(String college) {
+            this.college = college;
+      }
+}
