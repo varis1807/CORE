a933e47765822dbdaf2836785870869522d35856
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 06:14:25 +0530

    commit 22

--- OOPS/Encapsulation/student.java
+++ OOPS/Encapsulation/student.java
@@ -1,8 +1,13 @@
 import java.util.*;
+
 public class student {
       private String name;
-      public String getName(){
+
+      public String getName() {
             return name;
       }
-      
+
+      public void setName(String name) {
+            this.name = name;
+      }
 }
