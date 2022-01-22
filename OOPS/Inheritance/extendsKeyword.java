0401a85e9537937ed8a32d22aaf702dd013e0b3e
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 06:34:39 +0530

    commit 22

--- OOPS/Inheritance/extendsKeyword.java
+++ OOPS/Inheritance/extendsKeyword.java
@@ -1,4 +1,11 @@
 import java.util.*;
+
 public class extendsKeyword {
-      
+      static class employee {
+            float salary = 40000;
+      }
+
+      static class programme extends employee {
+            int bonus = 10000;
+      }
 }
