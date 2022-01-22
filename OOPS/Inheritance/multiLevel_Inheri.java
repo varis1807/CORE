61e85cec610e19779c666d7f6cb79c1afc154b5b
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 06:54:43 +0530

    commit 22

--- OOPS/Inheritance/multiLevel_Inheri.java
+++ OOPS/Inheritance/multiLevel_Inheri.java
@@ -1,3 +1,23 @@
 public class multiLevel_Inheri {
-      
+      static class A {
+            public static void func1() {
+                  System.out.println("this is the cls A");
+            }
+      }
+
+      static class B extends A {
+            public static void func2() {
+                  System.out.print("this is cls B");
+            }
+      }
+
+      static class C extends B {
+
+      }
+
+      public static void main(String[] args) {
+            C test = new C();
+            test.func1();
+            test.func2();
+      }
 }
--- OOPS/Inheritance/single_Inheritance.java
+++ OOPS/Inheritance/single_Inheritance.java
@@ -1,3 +1,16 @@
 public class single_Inheritance {
-      
+      static class A {
+            public static void func() {
+                  System.out.println("Inherited");
+            }
+      }
+
+      static class B extends A {
+      }
+
+      public static void main(String[] args) {
+            B test = new B();
+           B.func();
+      }
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
