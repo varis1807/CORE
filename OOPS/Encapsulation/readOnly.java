9e7fa40f886ace149837391557710877c6b25617
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 06:20:26 +0530

    commit 22

--- OOPS/Encapsulation/readOnly.java
+++ OOPS/Encapsulation/readOnly.java
@@ -1,8 +1,13 @@
 public class readOnly {
-      //private data member
-      private String clg="AGPCE";
-      //getter methode for clg
-      public String getCollege(){
+      // private data member
+      private String clg = "AGPCE";
+
+      // getter methode for clg
+      public String getCollege() {
             return clg;
+      }
+
+      public static void main(String[] args) {
+
       }
 }
