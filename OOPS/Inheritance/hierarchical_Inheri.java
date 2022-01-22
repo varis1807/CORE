7051fba6253685ae0ee51a023de1c9476b91e5da
Varis Rana <vrvarisrana123@gmail.com>
2022-01-22 07:02:02 +0530

    commit 22

--- OOPS/Inheritance/hierarchical_Inheri.java
+++ OOPS/Inheritance/hierarchical_Inheri.java
@@ -1,3 +1,22 @@
 public class hierarchical_Inheri {
-      
+      static class A {
+            public static void func() {
+                  System.out.println("this is the cls A");
+            }
+      }
+
+      static class B extends A {
+
+      }
+
+      static class C extends A {
+
+      }
+
+      public static void main(String[] args) {
+            B test1 = new B();
+            test1.func();
+            C test2 = new C();
+            test2.func();
+      }
 }
---
+++ OOPS/Inheritance/multiPle_inheri.java
@@ -0,0 +1,15 @@
+public class multiPle_inheri {
+      static class A {
+            public static void func1() {
+                  System.out.println("this is cls A");
+            }
+      }
+
+      static class B {
+            public static vid func2() {
+                  System.out.println("this is cls B");
+            }
+      }
+
+      
+}
